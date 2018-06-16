52f4861e7609f0be321e4bdc5e5b2d2cf8d1b06f
Konstantin Schmidt <konstantin@leafhouse.de>
2018-06-16 23:56:19 +0200

    Init CodeIgniter

--- snippets/PHP/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dir    = './';
-$files = array_diff(scandir($dir), array('..', '.', 'index.php'));
-echo "<h2>PHP</h2>";
-foreach($files as $file) {
-  echo "<a href='$file' target='_blank'>".ucfirst($file)."</a><br>";
-}
-?>
--- snippets/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dir    = './';
-$files = array_diff(scandir($dir), array('..', '.', 'index.php'));
-echo "<h2>Topics</h2>";
-foreach($files as $file) {
-  echo "<a href='$file' target='_blank'>".ucfirst($file)."</a><br>";
-}
-?>
