d3352f06134969e1c100ef9d3638432c8e1e75d3
Konstantin Schmidt <konstantin@leafhouse.de>
2018-06-16 16:17:27 +0200

    add little frontend

---
+++ snippets/index.php
@@ -0,0 +1,9 @@
+<?php
+include_once('../templates/template.php');
+$dir    = './';
+$files = array_diff(scandir($dir), array('..', '.', 'index.php'));
+echo "<h2>Topics</h2>";
+foreach($files as $file) {
+  echo "<a href='$file' target='_blank'>".ucfirst($file)."</a><br>";
+}
+?>
