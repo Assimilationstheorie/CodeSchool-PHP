2afcb60a6e92c35bf7d9a1e2fc40ce5305736a8f
Konstantin Schmidt <konstantin@leafhouse.de>
2018-06-18 19:15:05 +0200

    add local bootstrap

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title><?=$title;?></title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+  <link rel="stylesheet" href="<?=base_url();?>assets/css/bootstrap.min.css">
 </head>
 <body>
   <?php if($title != 'Welcome') { ?>
