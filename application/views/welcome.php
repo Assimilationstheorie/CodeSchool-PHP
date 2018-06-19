d82487ecbffc253554b248a5f79e7055d474a64d
Konstantin Schmidt <konstantin@leafhouse.de>
2018-06-19 20:12:48 +0200

    Delet the Project for rebuild!

--- application/views/welcome.php
+++
@@ -1,8 +0,0 @@
-<div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Menü</h1>
-    <p class="lead"><a href="/referenzen">Referenzen</a></p>
-    <p class="lead"><a href="/anonymefunktionen">Anonyme Funktionen</a></p>
-    <p class="lead"><a href="/klassen">Klassen</a></p>
-  </div>
-</div>
